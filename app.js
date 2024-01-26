72ddf6cf4b112dbac83e49162f8b38b95e6afb5e
Ítalo Xavier <italogax13@gmail.com>
2024-01-26 03:57:46 -0300

    "Adicionada Funcionalidade de Backend para o Formulário de Contato
    - Instalado e configurado o body-parser para processar dados de formulários.
    - Criada rota POST '/enviar-formulario' para lidar com os dados do formulário de contato.
    - Implementado recebimento e processamento básico dos dados do formulário.
    - Atualizado o formulário HTML para enviar dados para a nova rota do servidor.
    
    Essas mudanças permitem que o servidor Express receba e processe os dados enviados pelo formulário de contato, facilitando a interação do usuário com o site."

--- app.js
+++ app.js
@@ -15,3 +15,18 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+//Body-Parser
+const bodyParser = require('body-parser');
+
+//Configuração do body-parser
+app.use(bodyParser.urlencoded({ extended: true}));
+
+//Criando rotass POST para o formulário
+app.post('/enviar-formulario', (req, res) => {
+    const dadosFormulario = req.body;
+    console.log(dadosFormulario); //Aqui os dados são processados.
+    
+    //Enviando resposta para o frontend, como uma página de agradecimento ou uma mensagem de confirmação.
+    res.send('Formulário recebido');
+})
