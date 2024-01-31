c73006601cc953712892864394628069f45a94c5
Ítalo Xavier <italogax13@gmail.com>
2024-01-31 05:07:55 -0300

    Atualizei app.py para aprimorar o processamento de dados do formulário. Implementei funcionalidade AJAX para comunicação assíncrona entre o frontend e o backend. Removi a integração com SendGrid e substituí por uma configuração SMTP utilizando smtplib, adaptando o servidor para enviar e-mails através do Zoho Mail. Realizei testes para assegurar a correta funcionalidade da nova configuração de envio de e-mails e a comunicação eficiente entre o formulário e o servidor Flask.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify, render_template
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 
-#Configuração do SendGrid
-sg = SendGridAPIClient('mlsn.52ba50e0ee5615dca96c2a665b84facb0b9bfb3934889f7865503684c2a19666')
+# Configurações do servidor.
+smtp_server = "smtp.zoho.com"
+smtp_port = 587
+smtp_user = "italogax@zohomail.com"
+smtp_password = "C9zwJHSBAAKM"
 
 #Rota Raiz par servir a página inicial
 @app.route('/')
@@ -14,27 +18,26 @@ def index():
 
 #Rota para receber dados do formulário e enviar e-mail
 @app.route('/enviar-formulario', methods=['POST'])
-def enviar_email():
-    data = request.form
-    nome = data.get('nome')
-    email = data.get('email')
-    mensagem = data.get('mensagem')
+def enviar_formulario():
+    nome = request.form['nome']
+    email = request.form['email']
+    mensagem = request.form['mensagem']
 
-#Criar o objeto de e-mail
-    mail = Mail(
-        from_email='italogax13@gmail.com',
-        to_emails='italogax13@gmail.com',
-        subject='Teste site',
-        html_content=f'<strong>Nome: {nome}<br>Email: {email}<br>Mensagem: {mensagem}</strong> '
-    )
+    #Configurando a mensagem de e-mail
+    email_msg = MIMEMultipart()
+    email_msg['From'] = smtp_user
+    email_msg['To'] = "italogax13@gmail.com"
+    email_msg['Subject'] = "Novo Contato do Site"
+    corpo = f"Nome: {nome}\nE-mail: {email}\nMensagem: {mensagem}"
+    email_msg.attach(MIMEText(corpo, 'plain'))
+    
+    # Envia o e-mail
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_user, smtp_password)
+        server.sendmail(smtp_user, "italogax13@gmail.com", email_msg.as_string())
 
-#Enviar o e-mail
-    try:
-        response = sg.send(mail)
-        return jsonify({'message': 'E-mail enviado com sucesso!'}), 200
-    except Exception as e:
-        print(e)
-        return jsonify({'error': 'Errp ap emvoar p e-mail'}), 500
+    return jsonify({'mensagem': 'E-mail enviado com sucesso!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
